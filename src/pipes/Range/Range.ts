e734fb268bbc8e0b9c031645822719abd6a240d9
coryshaw1 <cory.shaw.dev@gmail.com>
2016-03-22 10:59:50 -0400

    feat(Range): Add ability to set the step amount for Range pipe

--- src/pipes/Range/Range.ts
+++ src/pipes/Range/Range.ts
@@ -10,14 +10,15 @@ import {Pipe, PipeTransform} from "angular2/core";
     pure: false
 })
 export class Range implements PipeTransform {
-    transform(value:any, config:any = [0, 4]){
+    transform(value:any, config:any = [0, 4, 1]){
 
         var newValue:any[] = [];
 
         var min = parseInt(config[0]);
         var max = parseInt(config[1]);
+        var step = parseInt(config[2]);
 
-        for (var i = min; i <= max; i++)
+        for (var i = min; i <= max; i += step)
             newValue.push(i);
 
         return newValue;
