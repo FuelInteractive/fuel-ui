ea9895b54652e494f75382c8b17054f033905b61
pavel.despodov <pavel.despodov@fuelinteractive.com>
2016-04-13 05:22:51 -0400

    Slider changes V1

---
+++ src/components/demoComponents.ts
@@ -0,0 +1,40 @@
+import {ALERT_DEMO_PROVIDERS, AlertDemo} from "./Alert/Alert.Demo";
+import {CAROUSEL_DEMO_PROVIDERS, CarouselDemo} from "./Carousel/Carousel.Demo";
+import {COLLAPSE_DEMO_PROVIDERS, CollapseDemo} from "./Collapse/Collapse.Demo";
+import {DATEPICKER_DEMO_PROVIDERS, DatePickerDemo} from "./DatePicker/DatePicker.Demo";
+import {DATERANGEPICKER_DEMO_PROVIDERS, DateRangePickerDemo} from "./DatePicker/DateRangePicker.Demo";
+import {DROPDOWN_DEMO_PROVIDERS, DropdownDemo} from "./Dropdown/Dropdown.Demo";
+import {INFINITESCROLLER_DEMO_PROVIDERS, InfiniteScrollerDemo} from "./InfiniteScroller/InfiniteScroller.Demo";
+import {MODAL_DEMO_PROVIDERS, ModalDemo} from "./Modal/Modal.Demo";
+import {PAGINATION_DEMO_PROVIDERS, PaginationDemo} from "./Pagination/Pagination.Demo";
+import {PROGRESS_DEMO_PROVIDERS, ProgressDemo} from "./Progress/Progress.Demo";
+import {TABLESORTABLE_DEMO_PROVIDERS, TableSortableDemo} from "./TableSortable/TableSortable.Demo";
+import {SLIDER_DEMO_PROVIDERS, SliderDemo} from "./Slider/Slider.Demo";
+
+export var FUELUI_DEMO_COMPONENT_PROVIDERS = [
+	ALERT_DEMO_PROVIDERS,
+    CAROUSEL_DEMO_PROVIDERS,
+    COLLAPSE_DEMO_PROVIDERS,
+    DATEPICKER_DEMO_PROVIDERS,
+    DATERANGEPICKER_DEMO_PROVIDERS,
+    DROPDOWN_DEMO_PROVIDERS,
+    INFINITESCROLLER_DEMO_PROVIDERS,
+    MODAL_DEMO_PROVIDERS,
+    PAGINATION_DEMO_PROVIDERS,
+    PROGRESS_DEMO_PROVIDERS,
+    TABLESORTABLE_DEMO_PROVIDERS,
+    SLIDER_DEMO_PROVIDERS
+];
+
+export * from "./Alert/Alert.Demo";
+export * from "./Carousel/Carousel.Demo";
+export * from "./Collapse/Collapse.Demo";
+export * from "./DatePicker/DatePicker.Demo";
+export * from "./DatePicker/DateRangePicker.Demo";
+export * from "./Dropdown/Dropdown.Demo";
+export * from "./InfiniteScroller/InfiniteScroller.Demo";
+export * from "./Modal/Modal.Demo";
+export * from "./Pagination/Pagination.Demo";
+export * from "./Progress/Progress.Demo";
+export * from "./TableSortable/TableSortable.Demo";
+export * from "./Slider/Slider.Demo";
