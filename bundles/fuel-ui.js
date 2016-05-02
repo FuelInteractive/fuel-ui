cbfc7f76980cd5a35bc7931530a9a9c8717e106d
coryshaw1 <cory.shaw.dev@gmail.com>
2016-05-02 13:54:07 -0400

    fix(DatePickers): Fix Firefox compatibility

--- bundles/fuel-ui.js
+++ bundles/fuel-ui.js
@@ -434,7 +434,7 @@ System.registerDynamic("fuel-ui/dist/components/DatePicker/DatePicker", ["angula
     };
     DatePicker.prototype.showCalendar = function(event) {
       if (event != null) {
-        var clickedRect = event.srcElement.parentElement.getBoundingClientRect();
+        var clickedRect = event.target ? event.target.parentElement.getBoundingClientRect() : event.srcElement.parentElement.getBoundingClientRect();
         this.calendarX = clickedRect.left;
         if (screen.height - clickedRect.bottom <= 500) {
           this.calendarY = (clickedRect.top);
