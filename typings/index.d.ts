4a5c3ef13c070fbb595859f2baaeada3cf2449fc
Stacy Gay <stacygay@gmail.com>
2016-06-22 09:57:09 -0400

    fix(slider): Include slider typings so they don't have to be installed explicitly when consuming fuel-ui

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/hammerjs/index.d.ts" />
-/// <reference path="globals/nouislider/index.d.ts" />
 /// <reference path="modules/es6-promise/index.d.ts" />
