f7774a0a66059e4b7d24ef8add822a8b737fe106
coryshaw1 <cory.shaw.dev@gmail.com>
2016-12-01 17:20:27 -0500

    fix(tooltip): Fix tooltip demo to use text parameter

--- src/directives/Tooltip/Tooltip.Demo.ts
+++ src/directives/Tooltip/Tooltip.Demo.ts
@@ -6,23 +6,23 @@ import {Attribute, AttributeColumns, AttributesDefaultSort} from '../../utilitie
   templateUrl: "Tooltip.Demo.html"
 })
 export class TooltipDemo {
-    codeExample1 = `&lt;a href=&quot;#&quot; tooltip=&quot;Tooltip text goes here.&quot; 
+    codeExample1 = `&lt;a href=&quot;#&quot; tooltip text=&quot;Tooltip text goes here.&quot;
     [position]=&quot;position&quot; [color]=&quot;color&quot; [size]=&quot;size&quot;
     [rounded]=&quot;rounded&quot; [always]=&quot;always&quot;&gt;
     Some text here.
 &lt;/a&gt;
 
 &lt;div class=&quot;col-md-4 form-group&quot;&gt;
-    &lt;div tooltip=&quot;You have entered: {{tooltipText}}!&quot; 
+    &lt;div tooltip text=&quot;You have entered: {{tooltipText}}!&quot;
                 [position]=&quot;position&quot; [color]=&quot;color&quot; [size]=&quot;size&quot;
                 [rounded]=&quot;rounded&quot; [always]=&quot;always&quot;&gt;
-        &lt;input [(ngModel)]=&quot;tooltipText&quot; type=&quot;text&quot; class=&quot;form-control&quot; placeholder=&quot;Tooltip text&quot; /&gt; 
+        &lt;input [(ngModel)]=&quot;tooltipText&quot; type=&quot;text&quot; class=&quot;form-control&quot; placeholder=&quot;Tooltip text&quot; /&gt;
     &lt;/div&gt;
     &lt;small class=&quot;text-muted&quot;&gt;Enter text above, then hover the input.&lt;/small&gt;
 &lt;/div&gt;`;
 
     attributes:Attribute[] = [
-        new Attribute('tooltip', 'string', 'null', 'Text of the tooltip'),
+        new Attribute('text', 'string', 'null', 'Text of the tooltip'),
         new Attribute('position', 'string', 'top', "Position of the tooltip compared to the element. Allows for: 'bottom-right', 'bottom', 'bottom-left', 'right', 'left', 'top-right,' 'top', or 'top-left'"),
         new Attribute('color', 'string', 'none', "Color of tooltip. Allows for: 'error', 'info', 'success', and 'warning'. Using anything else, include 'none', will result in the default black background with white text"),
         new Attribute('rounded', 'boolean', 'false', 'Rounded edges of tooltip'),
