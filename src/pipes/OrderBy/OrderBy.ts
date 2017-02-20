e6a6196c39a0837ac8f50a2c1a950c8e6e27f0f0
Giorgio Fellipe <giorgiofellipe@gmail.com>
2017-02-20 11:15:21 -0300

    Check if the comparison is between boolean or Date
    This should fix #99

--- src/pipes/OrderBy/OrderBy.ts
+++ src/pipes/OrderBy/OrderBy.ts
@@ -16,13 +16,17 @@ export class OrderByPipe implements PipeTransform {
 
         if(a === null || typeof a === 'undefined') a = 0;
         if(b === null || typeof b === 'undefined') b = 0;
-
-		if((isNaN(parseFloat(a)) || !isFinite(a)) || (isNaN(parseFloat(b)) || !isFinite(b))){
+		
+		if ((typeof a == 'boolean' || typeof b == 'boolean') || (a instanceof Date || b instanceof Date)) {
+			if (a < b) return -1;
+			if (a > b) return 1;
+		} 
+		else if((isNaN(parseFloat(a)) || !isFinite(a)) || (isNaN(parseFloat(b)) || !isFinite(b))){
 			//Isn't a number so lowercase the string to properly compare
 			if(a.toLowerCase() < b.toLowerCase()) return -1;
 			if(a.toLowerCase() > b.toLowerCase()) return 1;
 		}
-		else{
+		else {
 			//Parse strings as numbers to compare properly
 			if(parseFloat(a) < parseFloat(b)) return -1;
 			if(parseFloat(a) > parseFloat(b)) return 1;
